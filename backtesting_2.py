a13ff153a4c88c4059f9ea4f27208922fc80a1c2
marianoradusky.19 <marianoradusky.19@gmail.com>
2021-10-13 00:02:06 -0300

    Se añaden estrategias para testear

--- backtesting_2.py
+++ backtesting_2.py
@@ -2,7 +2,7 @@ import backtrader as bt
 import yfinance as yf
 import strategies
 
-ticker = 'ADA-USD'
+ticker = 'BTC-USD'
 start =  '2021-01-01'
 end = '2021-10-11'
 df = yf.download(ticker,start, end)
@@ -10,7 +10,7 @@ df.to_csv('datatester.csv')
 
 
 cerebro = bt.Cerebro()
-cerebro.addstrategy(strategies.BOLLStrat)
+cerebro.addstrategy(strategies.emaPriceCross)
 cerebro.broker.set_cash(100000)
 
 
